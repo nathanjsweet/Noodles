83bc3d9f481cc112b2c1f50c7e36d00072b758bb
Nathan Sweet <nathanjsweet@gmail.com>
2012-04-18 13:33:54 -0700

    added globalization support.

---
+++ plugins/iftag/globalization.js
@@ -0,0 +1,9 @@
+if(typeof process !== "undefined"){
+	var requirejs = require('requirejs');
+	var define = requirejs.define;
+	var _Noodles = require('./../lib');
+}
+
+define(function(require, exports, module){
+exports.languages = {'english':{}};
+});
