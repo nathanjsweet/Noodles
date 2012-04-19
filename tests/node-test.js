a5232c65590237b0c1171e8a0aae81fdc9a55d8c
Nathan Sweet <nathanjsweet@gmail.com>
2012-04-18 17:08:45 -0700

    updating tests

--- tests/node-test.js
+++ tests/node-test.js
@@ -6,8 +6,8 @@ var rawString = ['This is a very basic template. By <{author}>.',
 	'<{if middlename}>, his middlename is <{middlename}><{end}>',
 	' his last name is <{lastname}>',
 '<{end}>',
-'<{if authors or firstname}>',
-'<{loop authors}>',
+'<{if authors}>',
+'<{loop !authors}>',
 	'index: <{__index}>',
 	'name:<{name}>',
 	'value:<{value}>',
@@ -15,21 +15,62 @@ var rawString = ['This is a very basic template. By <{author}>.',
 '<{end}>'].join('\n');
 
 var metaData = 'author = <{firstName}> <{lastName}>';
-var date = Date.now(),
-myTemp;
+var time = Date.now(),
+	object = {'lastname':'Sweet','firstname':'Nathan','authors':["location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp","location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp","location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp","location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp","location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp"]},
+	object2 = {'lastname':'Sweet','firstname':'Nathan','authors':["location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp","location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp","location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp","location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp","location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp"]},
+	myTemp;
 new Noodles.Template({
 	rawString : rawString,
 	metaData:metaData,
-	onFinishCompiling:function(template){
-		console.log('compile time: ',Date.now()-date,'ms.');
-		console.log(template);
+	onFinishCompiling:function(template){s
+		time = Date.now() - time;
 		myTemp = template;
-		date = Date.now();
-		template.execute({'lastname':'Sweet','firstname':'Nathan','authors':["location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp","location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp","location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp","location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp","location", "top", "window", "external", "chrome", "v8Locale", "document", "requirejs", "require", "define", "_exports", "_parseConditional", "_Template", "identifier", "plugin", "myTemp"]},{
-			onRenderAll:function(string){
-				console.log(Date.now() - date);
-				console.log(string);
-			}
-		});
+		console.log('compile time: ',time,'ms');
+		testTemp(myTemp,10000);
 	}
 });
+
+function testTemp(temp,length){
+	var arr = [],
+		i = length;
+	while(i--){
+		if((i & 1) === 1){
+			temp.execute(object,{
+				onRenderAll:function(string){if(i === 0)console.log(string);},
+				onFinish:function(time){
+					arr.push(time);
+				}
+			});
+		}
+		else{
+			temp.execute(object2,{
+				onRenderAll:function(string){if(i === 0)console.log(string);},
+				onFinish:function(time){
+					arr.push(time);
+				}
+			});
+		}
+	}
+	setTimeout(logTime,1000,arr);
+};
+
+function logTime(arr){
+	var totalTime = 0,
+		upperBound = 0,
+		lowerBound = Number.POSITIVE_INFINITY;
+	for(var i = 0, l = arr.length; i < l; i++){
+		if(typeof arr[i] !== "number"){
+			setTimeout(logTime,1000,arr);
+			return;
+		}
+		if(arr[i] > upperBound) upperBound = arr[i];
+		if(arr[i] < lowerBound) lowerBound = arr[i];
+		totalTime += arr[i];
+	}
+	console.log('The average time was: ', totalTime/l,'ms.\n',
+	'The upper bound time was: ', upperBound, 'ms.\n',
+	'The lower bound time was: ', lowerBound,'ms.'
+	);
+	
+		
+}
