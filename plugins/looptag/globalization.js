c7bec65ddcaab28dbb45af904c495ccb2022aa32
Nathan Sweet <nathanjsweet@gmail.com>
2012-05-14 20:37:18 -0700

    Renamed globalization files to be tag specific in order to accomodate require js compression

--- plugins/looptag/globalization.js
+++
@@ -1,9 +0,0 @@
-if(typeof process !== "undefined"){
-	var requirejs = require('requirejs');
-	var define = requirejs.define;
-	var _Noodles = require('./../lib');
-}
-
-define(function(require, exports, module){
-exports.languages = {'english':{}};
-});
