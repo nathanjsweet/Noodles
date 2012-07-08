c8cfa7576f3e15a78b6e3f2fd73115dae0bf6516
Nathan Sweet <nathanjsweet@gmail.com>
2012-07-08 15:37:30 -0700

    added new language specific globalization files

---
+++ plugins/iftag/iftag_globalization.french.js
@@ -0,0 +1,8 @@
+if(typeof process !== "undefined"){
+	var define = require.define;
+	var _Noodles = require('./../lib/noodles.js');
+}
+
+define(function(require, exports, module){
+exports.language = {};
+});
