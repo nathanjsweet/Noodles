b422fb491c5a139a5fde93d63e480edf6f804102
Nathan Sweet <nathanjsweet@gmail.com>
2012-09-27 17:39:46 -0700

    no changes, just meta

--- plugins/exittag/exittag_globalization.english.js
+++ plugins/exittag/exittag_globalization.english.js
@@ -2,7 +2,6 @@ if(typeof process !== "undefined"){
 	var define = require.define;
 	var _Noodles = require('./../lib/noodles.js');
 }
-
 define(function(require, exports, module){
 exports.language = {
 	tag:{
